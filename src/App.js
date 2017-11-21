4ad3dc11c669438bb1b0462435966faec592a386
jessicasalmon <jessicasalmon05@gmail.com>
2017-11-21 10:55:13 +0000

    render buildingDetails components update state with building info relates #10

--- src/App.js
+++ src/App.js
@@ -2,33 +2,46 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import Map from './Map.js';
+import BuildingDetails from './BuildingDetails.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       buildings: props.data.getBuildings,
+      building: {},
       error: props.data.error,
       loading: props.data.loading
     }
   }
 
-// check if the next prop loading === false
-// if false, update the state
-componentWillUpdate(nextProps, nextState) {
-  if (nextProps.data.loading === false && !nextState.buildings) {
+  // check if the next prop loading === false
+  // if false, update the state
+  componentWillUpdate(nextProps, nextState) {
+    if (nextProps.data.loading === false && !nextState.buildings) {
+      this.setState({
+        buildings: nextProps.data.getBuildings,
+        error: nextProps.data.error,
+        loading: nextProps.data.loading
+      })
+    }
+  }
+
+   buildingDetails = (clickHandlerObj, building) => {
     this.setState({
-      buildings: nextProps.data.getBuildings,
-      error: nextProps.data.error,
-      loading: nextProps.data.loading
+      building: building
     })
   }
-}
+
   render() {
     console.log(this.state, '<<<<')
     return (
       <div>
-        <Map buildings={ this.state.buildings } />
+        <Map
+        buildings={ this.state.buildings }
+        handleBuildingDetails={ this.buildingDetails } />
+        <BuildingDetails
+        building={ this.state.building }/>
       </div>
     );
   }
