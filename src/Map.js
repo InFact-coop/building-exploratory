3fc6deef1e0726030242a72c52e38d83a21bcf94
sohilpandya <sohilpandya@me.com>
2017-12-11 12:49:27 +0000

    using ubers map-gl libary to test it out

--- src/Map.js
+++ src/Map.js
@@ -1,30 +1,86 @@
 import React, { Component } from 'react';
-import ReactMapboxGL, { Layer, Feature } from 'react-mapbox-gl';
+// import ReactMapboxGL, { Layer, Feature } from 'react-mapbox-gl';
 import MapLayer from './MapLayer';
+import ReactMapGL, { Marker, NavigationControl, FlyToInterpolator } from 'react-map-gl';
 
-const Mapbox = ReactMapboxGL({
-  accessToken: 'pk.eyJ1Ijoic29oaWxwYW5keWEiLCJhIjoiY2phODdiMnM1MDQybjMycGZ3ZTE0d3RsOCJ9.4WBBpoMgECNDbRL4BahGhQ'
-});
+// const Mapbox = ReactMapboxGL({
+//   accessToken: 'pk.eyJ1Ijoic29oaWxwYW5keWEiLCJhIjoiY2phODdiMnM1MDQybjMycGZ3ZTE0d3RsOCJ9.4WBBpoMgECNDbRL4BahGhQ'
+// });
 
 class Map extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      viewport: {
+        latitude: this.props.mapCenter[1],
+        longitude: this.props.mapCenter[0],
+        zoom: 11.5,
+        bearing: 0,
+        pitch: 0,
+        width: 500,
+        height: 500,
+      },
+      popupInfo: null
+    };
   }
 
+  _updateViewport = (viewport) => {
+    this.setState({ viewport });
+  }
+
+  _goToViewport = ({ longitude, latitude }) => {
+    this._onViewportChange({
+      longitude,
+      latitude,
+      zoom: 11,
+      transitionInterpolator: new FlyToInterpolator(),
+      transitionDuration: 3000
+    });
+  }
+
+
   render() {
-    const { buildings, handleBuildingDetails, mapCenter, mapZoom } = this.props;
+
+    const { buildings = [], handleBuildingDetails, mapCenter, mapZoom } = this.props;
+    const { viewport } = this.state;
+
+    const allMarkers = buildings.length > 0 && buildings.map((buildingObj, i) => {
+      console.log('hello', buildingObj.longitude, buildingObj.latitude)
+      return (
+        <Marker
+          key={i}
+          longitude={buildingObj.longitude}
+          latitude={buildingObj.latitude}
+          onClick={(obj) => {
+            handleBuildingDetails(obj, buildingObj);
+          }}
+        >
+          <div
+            onMouseEnter={(obj) => {
+              console.log('entered');
+            }}
+            onMouseLeave={(obj) => {
+              console.log('left');
+            }}
+          >
+          a
+          </div>
+        </ Marker>
+      );
+    });
+
     return (
-      <Mapbox
-      style="mapbox://styles/sohilpandya/cja87dmin0ct62sl4jxyo4tzp"
-      center={ mapCenter }
-      zoom={ mapZoom }
-      containerStyle={{ height: "100vh", width: "100%" }}
-      movingMethod={'flyTo'}>
-        <MapLayer
-          { ...this.props }
-          />
-      </Mapbox>
+      <ReactMapGL
+          { ...viewport }
+          mapboxApiAccessToken={'pk.eyJ1Ijoic29oaWxwYW5keWEiLCJhIjoiY2phODdiMnM1MDQybjMycGZ3ZTE0d3RsOCJ9.4WBBpoMgECNDbRL4BahGhQ'}
+          mapStyle='mapbox://styles/sohilpandya/cja87dmin0ct62sl4jxyo4tzp'
+          onViewportChange={this._goToViewport}
+        >
+        { buildings.length > 0 && allMarkers }
+
+        <NavigationControl onViewportChange={this._updateViewport} />
+        </ReactMapGL>
     )
   }
 }
