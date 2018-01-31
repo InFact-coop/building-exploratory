de687af88f1d6a3e0aed661ce31b5a40f3dea932
sohilpandya <sohilpandya@me.com>
2018-01-31 16:59:37 +0000

    added core-js reflect polyfill related to #58

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AppContainer from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import 'core-js/fn/array/find';
+import 'core-js/fn/reflect';
 
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
