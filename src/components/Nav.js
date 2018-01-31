ccc9c70338fbbfea22e058d143ab0d80702deb9a
jessicasalmon <jessicasalmon05@gmail.com>
2018-01-31 21:29:14 +0000

    home icon links to home page relates #59

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import NavHome from './svg/NavHome';
 
-const Nav = ({ handleIsAboutPage, isAbout }) => {
+const Nav = ({ handleIsHomePage, handleIsAboutPage, isAbout }) => {
   return (
     <nav className="float w-100 h3 flex justify-between items-center bb b--primary bw1">
       <div className="flex items-center">
-        <div className="h3 w3 bg-primary flex justify-center items-center">
+        <div className="h3 w3 bg-primary flex justify-center items-center" onClick = { () => { handleIsHomePage(); }}>
           <NavHome />
-      </div>
+        </div>
         <p className="f2-l f4 pt1 ma0 pl2 primary fw8"> Islington Local List </p>
       </div>
 
