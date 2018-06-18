6b5b5b0bc16b0d3eef23247dd6f204964ea47f26
jessicasalmon <jessicasalmon05@gmail.com>
2018-06-18 22:49:43 +0100

    tidy

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,27 +11,20 @@ const About = () => {
   return(
     <main className="mv5 mv0-l flex-l primary">
 
-      <div
-        className="w-50-l ph4 pv5-l height-scroll-l overflow-scroll-l">
-
+      <div className="w-50-l ph4 pv5-l height-scroll-l overflow-scroll-l">
         <header>
           <img alt="The Building Exploratory" src={logo} className="measure-narrow-l w-4" />
           <p className="f2 b mt2 mb0">Islington Local List</p>
         </header>
-
         <IslingtonLocalList />
-
         <OurEyesOnIslington />
-
         <BuildingExploratory />
-          <Partners />
+        <Partners />
       </div>
 
-      <div
-        className="w-50-l ph4 pv5-l bl-l b--primary bw1-l height-scroll-l overflow-scroll-l">
-          <Volunteers />
+      <div className="w-50-l ph4 pv5-l bl-l b--primary bw1-l height-scroll-l overflow-scroll-l">
+        <Volunteers />
       </div>
-
 
     </main>
   )
